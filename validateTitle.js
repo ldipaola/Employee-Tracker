ee70dfb880fe73a06075ab955f1f160591da7704
Lukey <ldipaola66@gmail.com>
2020-08-03 23:33:04 +1000

    removed, file name changed

--- validateTitle.js
+++
@@ -1,9 +0,0 @@
-const validateTitle = (title) => {
- 
-      if (title.trim() === "") {       
-        return 'Title must not be empty!'; 
-      }
-      return true;
-  }
-
-module.exports = validateTitle;
